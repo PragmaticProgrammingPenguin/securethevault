22f9b3a335b38ca2e68626de96e32106b09bb52e
PragmaticProgrammingPenguin <honeycuttmichael5@gmail.com>
2024-05-13 13:23:25 -0400

    Adding project files

---
+++ block7/secureTheVault.js
@@ -0,0 +1,10 @@
+//Create message
+const message="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//Declare variables
+const first=5+5;
+const second=4*10;
+const third= 49-10;
+
+//Launch alert
+window.alert(message + ' ' + first + ' ' + second + ' ' + third);
